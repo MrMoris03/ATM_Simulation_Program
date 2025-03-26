4a8a7b5af1cf7932f8642a8b954f12dd6d2de789
MrMoris03 <yusuphmoris03@gmail.com>
2025-03-26 21:16:17 +0300

    comment

--- ATM_Simulation_Program.cpp
+++ ATM_Simulation_Program.cpp
@@ -7,8 +7,8 @@ using namespace std;
 // Account class
 class Account{
 	private:
-		string accountHolder; //Person with access to current account
-		string pin;
+		string accountHolder;	//Person with access to current account
+		string pin;	//Pin to authorize account access
 		double balance;
 		vector<string> transactionHistory;
 		
