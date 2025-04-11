82f2b7aaa3629f87f2974b2c74dd0656fb8dc8e3
MrMoris03 <yusuphmoris03@gmail.com>
2025-04-11 16:03:43 +0300

    Update ATM_Simulation_Program.cpp

--- ATM_Simulation_Program.cpp
+++ ATM_Simulation_Program.cpp
@@ -51,6 +51,6 @@ class Account {
 
 		void viewTransactionHistory() {
 			cout << "Transaction History:" << endl;
-			for (const auto &transaction : transctionHistory)
+			for (const auto &transaction : transctionHistory) 
 		}
 };
