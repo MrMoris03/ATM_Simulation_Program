6c80b2b80959cc55a8cda3707c91f57d7ef1a4b9
MrMoris03 <yusuphmoris03@gmail.com>
2025-04-21 21:42:18 +0300

    Update ATM_Simulation_Program.cpp

--- ATM_Simulation_Program.cpp
+++ ATM_Simulation_Program.cpp
@@ -69,6 +69,6 @@ class ATM {
 		}
 		
 		void addAccount(string holder, string pin, double initialBalance) {
-			accounts.emplace_back(holder, pin, ini)
+			accounts.emplace_back(holder, pin, initial)
 		}
 };
