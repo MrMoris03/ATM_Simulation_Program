c372727a7d54668b8ff584860fb85091b6ebca8c
MrMoris03 <yusuphmoris03@gmail.com>
2025-05-14 10:50:55 +0300

    Update ATM_Simulation_Program.cpp

--- ATM_Simulation_Program.cpp
+++ ATM_Simulation_Program.cpp
@@ -108,7 +108,7 @@ class ATM {
 				
 				switch (choice) {
 					case 1:
-						cout << "Current Balance: $" << currentAccount -> c
+						cout << "Current Balance: $" << currentAccount -> ch
 				}
 			}
 		}
