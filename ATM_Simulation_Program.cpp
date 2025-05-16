4d02837da3cd8ab4aa4b46f2c78a6607417b4da5
MrMoris03 <yusuphmoris03@gmail.com>
2025-05-16 15:27:16 +0300

    Update ATM_Simulation_Program.cpp

--- ATM_Simulation_Program.cpp
+++ ATM_Simulation_Program.cpp
@@ -111,7 +111,7 @@ class ATM {
 						cout << "Current Balance: $" << currentAccount -> checkBalance() << endl;
 						break;
 					case 2: {
-						double depositA
+						double depositAm
 						break;
 					}
 						
