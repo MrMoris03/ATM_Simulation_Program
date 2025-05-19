418d2f1be1f3aae04a226ca5859e0cc535e5c91d
MrMoris03 <yusuphmoris03@gmail.com>
2025-05-19 09:54:57 +0300

    Update ATM_Simulation_Program.cpp

--- ATM_Simulation_Program.cpp
+++ ATM_Simulation_Program.cpp
@@ -114,7 +114,7 @@ class ATM {
 						double depositAmount;
 						cout << "Enter amount to deposit: ";
 						cin >> depositAmount;
-						currentAccount->deposi
+						currentAccount->deposit
 						break;
 					}
 						
