3e79f4c5533519e87bb46cfff17756d73f04ae82
MrMoris03 <yusuphmoris03@gmail.com>
2025-05-24 10:17:56 +0300

    Update ATM_Simulation_Program.cpp

--- ATM_Simulation_Program.cpp
+++ ATM_Simulation_Program.cpp
@@ -121,7 +121,7 @@ class ATM {
 						double withdrawalAmount;
 						cout << "Enter amount to withdraw: ";
 						cin >> withdrawalAmount;
-						currentAccount->with
+						currentAccount->withd
 						break;
 					}	
 				}
